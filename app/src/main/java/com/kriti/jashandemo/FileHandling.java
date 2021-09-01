00c5bc1ae21ef92d72fc014feffa608d0ea834c5
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-09-01 09:58:05 +0530

    options menu

--- app/src/main/java/com/kriti/jashandemo/FileHandling.java
+++ app/src/main/java/com/kriti/jashandemo/FileHandling.java
@@ -1,11 +1,15 @@
 package com.kriti.jashandemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -72,5 +76,31 @@ public class FileHandling extends AppCompatActivity implements View.OnClickListe
         {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menuItem1)
+        {
+            Toast.makeText(this, "Menu Item 1", Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.menuItem2)
+        {
+            Toast.makeText(this, "Menu Item 2", Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.menuItem3)
+        {
+            Toast.makeText(this, "Menu Item 3", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
     }
 }
