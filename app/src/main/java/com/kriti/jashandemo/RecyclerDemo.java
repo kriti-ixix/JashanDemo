8baa90dd7060a8b0654f2deec4eece61d2b017ca
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-09-07 10:27:14 +0530

    recycler view

--- app/src/main/java/com/kriti/jashandemo/RecyclerDemo.java
+++ app/src/main/java/com/kriti/jashandemo/RecyclerDemo.java
@@ -2,9 +2,12 @@ package com.kriti.jashandemo;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,12 +20,28 @@ public class RecyclerDemo extends AppCompatActivity {
 
     RecyclerView recyclerView;
     ArrayList <RecyclerInfo> myList = new ArrayList<RecyclerInfo>();
+    MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_demo);
         recyclerView = findViewById(R.id.recyclerView);
+
+        for (int i=0; i<=5; i++)
+        {
+            RecyclerInfo info = new RecyclerInfo();
+            info.setrImage(i);
+            info.setrText(i);
+            myList.add(info);
+        }
+
+        adapter = new MyAdapter();
+        //LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
+
     }
 
     class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>
@@ -35,7 +54,9 @@ public class RecyclerDemo extends AppCompatActivity {
             public MyViewHolder(@NonNull View itemView) {
                 super(itemView);
                 textView = itemView.findViewById(R.id.recyclerTextView);
-
+                emptyHeartView = itemView.findViewById(R.id.recyclerEmptyHeart);
+                fullHeartView = itemView.findViewById(R.id.recyclerFullHeart);
+                imageView = itemView.findViewById(R.id.recyclerImageView);
             }
         }
 
@@ -48,7 +69,42 @@ public class RecyclerDemo extends AppCompatActivity {
         }
 
         @Override
-        public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
+        public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position)
+        {
+            RecyclerInfo info = myList.get(position);
+
+            holder.textView.setText(info.getrText());
+            holder.imageView.setImageResource(info.getrImage());
+
+            if (info.isrHeart())
+            {
+                holder.emptyHeartView.setVisibility(View.GONE);
+                holder.fullHeartView.setVisibility(View.VISIBLE);
+            }
+            else
+            {
+                holder.emptyHeartView.setVisibility(View.VISIBLE);
+                holder.fullHeartView.setVisibility(View.GONE);
+            }
+
+            holder.emptyHeartView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d("Listener", "Empty heart is clicked");
+                    info.setrHeart(true);
+                    adapter.notifyDataSetChanged();
+                }
+            });
+
+            holder.fullHeartView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d("Listener", "Full heart is clicked");
+                    info.setrHeart(false);
+                    adapter.notifyDataSetChanged();
+                }
+            });
+
         }
 
         @Override
--- app/src/main/java/com/kriti/jashandemo/RecyclerInfo.java
+++ app/src/main/java/com/kriti/jashandemo/RecyclerInfo.java
@@ -1,4 +1,32 @@
 package com.kriti.jashandemo;
 
-public class RecyclerInfo {
+public class RecyclerInfo
+{
+    int rImage; boolean rHeart = false; String rText = "";
+    int[] images = {R.drawable.one, R.drawable.two, R.drawable.three, R.drawable.four, R.drawable.five, R.drawable.six};
+    String[] text = {"One", "Two", "Three", "Four", "Five", "Six"};
+
+    public int getrImage() {
+        return rImage;
+    }
+
+    public void setrImage(int index) {
+        this.rImage = images[index];
+    }
+
+    public boolean isrHeart() {
+        return rHeart;
+    }
+
+    public void setrHeart(boolean rHeart) {
+        this.rHeart = rHeart;
+    }
+
+    public String getrText() {
+        return rText;
+    }
+
+    public void setrText(int index) {
+        this.rText = text[index];
+    }
 }
