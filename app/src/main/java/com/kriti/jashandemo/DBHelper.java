1621b8e0aa2ee8d75e14ca423a77386bc25367ed
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-09-09 17:39:26 +0530

    sqlite

---
+++ app/src/main/java/com/kriti/jashandemo/DBHelper.java
@@ -0,0 +1,39 @@
+package com.kriti.jashandemo;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper
+{
+    public static final String DATABASE = "school";
+    public static final String TABLE = "students";
+
+    public DBHelper(@Nullable Context context) {
+        super(context, DATABASE, null, 1);
+    }
+
+    long addValues(int rn, String n, float m)
+    {
+        SQLiteDatabase sq = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("rollno", rn);
+        cv.put("name", n);
+        cv.put("marks", m);
+        long result = sq.insert(TABLE, null, cv);
+        return result;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table " + TABLE + " (rollno integer primary key, name text, marks float)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
