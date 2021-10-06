9435e2944c06832505874b1bc13bcba068ff785f
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-10-06 13:45:51 +0530

    getting data

--- app/src/main/java/com/kriti/jashandemo/StudentsDisplay.java
+++ app/src/main/java/com/kriti/jashandemo/StudentsDisplay.java
@@ -3,6 +3,17 @@ package com.kriti.jashandemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class StudentsDisplay extends AppCompatActivity {
 
@@ -10,5 +21,42 @@ public class StudentsDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_students_display);
+
+        String url = "http://"+ getString(R.string.host) + "/jashandemo/select.php";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try
+                {
+                    JSONObject json = new JSONObject(response);
+                    String res = json.getString("res");
+
+                    if (res.equals("OK"))
+                    {
+
+                    }
+                    else
+                    {
+                        Toast.makeText(StudentsDisplay.this, "Empty", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(StudentsDisplay.this, "Error connecting", Toast.LENGTH_SHORT).show();
+                Log.d("Volley Error", error.getMessage());
+            }
+        });
+
+        queue.add(request);
+
     }
 }
