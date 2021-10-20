be3df45a8da51314782f097dc8cfd3b5c0c89b60
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-10-20 10:24:31 +0530

    Saving image to phone

--- app/src/main/java/com/kriti/jashandemo/GettingAnImage.java
+++ app/src/main/java/com/kriti/jashandemo/GettingAnImage.java
@@ -1,22 +1,34 @@
 package com.kriti.jashandemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class GettingAnImage extends AppCompatActivity {
 
-    Button button; ImageView imageView;
+    Button button; ImageView imageView; ProgressBar progressBar;
     String imageUrl = "https://images.unsplash.com/photo-1630313621450-9e90dcf99d09?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1587&q=80";
 
     @Override
@@ -26,6 +38,13 @@ public class GettingAnImage extends AppCompatActivity {
 
         button = findViewById(R.id.downloadButton);
         imageView = findViewById(R.id.downloadedImageView);
+        progressBar = findViewById(R.id.progressBar);
+
+        //Check for permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //Ask for permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        }
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +55,8 @@ public class GettingAnImage extends AppCompatActivity {
                     GetImage getImage = new GetImage();
                     Bitmap bitmap = getImage.execute(imageUrl).get();
                     imageView.setImageBitmap(bitmap);
+                    Log.d("Downloading", "Image downloaded");
+                    saveImage(bitmap);
                 }
 
                 catch (Exception e)
@@ -46,11 +67,40 @@ public class GettingAnImage extends AppCompatActivity {
         });
     }
 
+    void saveImage(Bitmap bitmap)
+    {
+        try
+        {
+            File sdCard = Environment.getExternalStorageDirectory();
+            File directory = new File(sdCard.getAbsolutePath() + "/Download");
+            //directory.mkdir();
+
+            File output = new File(directory, "my image.jpg");
+            FileOutputStream fos = new FileOutputStream(output);
+
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush(); fos.close();
+
+            MediaStore.Images.Media.insertImage(getContentResolver(), bitmap,
+                    "my image.jpg", null);
+
+            Log.d("Downloading", "Image saved");
+
+            Toast.makeText(this, "Download complete", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
     class GetImage extends AsyncTask<String, Void, Bitmap>
     {
         @Override
         protected Bitmap doInBackground(String... strings)
         {
+            progressBar.setVisibility(View.VISIBLE);
+
             try
             {
                 URL url = new URL(strings[0]);
@@ -66,6 +116,12 @@ public class GettingAnImage extends AppCompatActivity {
                 e.printStackTrace();
                 return null;
             }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            progressBar.setVisibility(View.INVISIBLE);
         }
     }
 }
